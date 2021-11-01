5721182e06d41acbcd8e46bcc7b0499782f836be
SimonZhan-code <simonzhan@berkeley.edu>
2021-10-31 18:01:17 -0700

    settup for second experiment

---
+++ experiment3/Assets/Laserpointer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Laserpointer : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Behaviour_Pose controllerPose;
+    public SteamVR_Action_Boolean teleportAction;
+    public SteamVR_Action_Boolean grabAction;
+    public GameObject targetsphere;
+
+    public GameObject laserPrefab;
+    private GameObject laser;
+    private Transform LaserTransform;
+    private Vector3 hitPoint;
+
+    void Start()
+    {
+        laser = Instantiate(laserPrefab);
+
+        LaserTransform = laser.transform;
+    }
+
+
+    void Update()
+    {
+        if (teleportAction.GetState(handType))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(controllerPose.transform.position, transform.forward, out hit, 100))
+            {
+                hitPoint = hit.point;
+                ShowLaser(hit);
+                if (GetGrab())
+                {
+                    Debug.Log(hit.collider.gameObject);
+                }
+            }
+        }
+        else
+        {
+            laser.SetActive(false);
+        }
+
+    }
+    private void ShowLaser(RaycastHit hit)
+    {
+        laser.SetActive(true);
+        LaserTransform.position = Vector3.Lerp(controllerPose.transform.position, hitPoint, .5f);
+        LaserTransform.LookAt(hitPoint);
+
+        LaserTransform.localScale = new Vector3(LaserTransform.localScale.x, LaserTransform.localScale.y, hit.distance);
+
+    }
+    public bool GetGrab()
+    {
+        return grabAction.GetState(handType);
+    }
+}
---
+++ experiment3/Assets/hands/Hand.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Hand : MonoBehaviour
+{
+    public SteamVR_Action_Boolean m_Grabaction = null;
+
+
+
+    private SteamVR_Behaviour_Pose m_Pose = null;
+    private FixedJoint m_Joint = null;
+
+    private Interactable m_CurrentInteractable = null;
+    public List<Interactable> m_ContactInterables = new List<Interactable>();
+
+    private void Awake()
+    {
+        m_Pose = GetComponent<SteamVR_Behaviour_Pose>();
+        m_Joint = GetComponent<FixedJoint>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //Down
+        if (m_Grabaction.GetStateDown(m_Pose.inputSource))
+        {
+            if (m_ContactInterables.Count != 0)
+                print(m_Pose.inputSource + " Trigger Down");
+                Pickup();
+        }
+        //Up
+        if (m_Grabaction.GetStateUp(m_Pose.inputSource))
+        {
+            if (m_ContactInterables.Count == 0)
+                print(m_Pose.inputSource + " Trigger Up");
+                Drop();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Interactable"))
+            m_ContactInterables.Add(other.gameObject.GetComponent<Interactable>());
+
+       
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Interactable"))
+            m_ContactInterables.Remove(other.gameObject.GetComponent<Interactable>());
+
+        
+    }
+
+    public void Pickup()
+    {
+
+    }
+
+    public void Drop()
+    {
+
+    }
+
+    private Interactable GetNearstInteractable()
+    {
+        return null;
+        
+    }
+}
