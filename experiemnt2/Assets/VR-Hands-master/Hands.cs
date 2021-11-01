042f497d4daf0111f8bea39c52ccbe65b0531a2f
SimonZhan-code <simonzhan@berkeley.edu>
2021-10-31 18:00:26 -0700

    settup for second experiment

---
+++ experiemnt2/Assets/VR-Hands-master/Hands.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Hand : MonoBehaviour
+{
+    public SteamVR_Action_Boolean m_GrabAction = null;
+
+    private SteamVR_Behaviour_Pose m_Pose = null;
+    private FixedJoint m_Joint = null;
+
+    private Interactable m_CurrentInteractable = null;
+    private List<Interactable> m_ContactInteractables = new List<Interactable>();
+
+    private void Awake()
+    {
+        m_Pose = GetComponent<SteamVR_Behaviour_Pose>();
+        m_Joint = GetComponent<FixedJoint>();
+    }
+
+     private void Update()
+    {
+        //Down
+
+        //Up
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+
+    public void Pickup()
+    {
+
+    }
+
+
+}
